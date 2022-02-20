d17521ee6b2761a638dc86fc1c07f67afd71551e
ateezunionbra <ateezunionreborn@gmail.com>
2022-02-20 19:02:50 -0300

    desire

---
+++ assets/js/rasc.js
@@ -0,0 +1,156 @@
+
+document.getElementsByClassName("tablink7")[0].click();
+
+function openPrograma7(evt, programa7Name) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("programa7");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink7");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(programa7Name).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+}
+
+
+
+document.getElementsByClassName("tablink8")[0].click();
+
+function openPrograma8(evt, programa8Name) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("programa8");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink8");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(programa8Name).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+}
+
+
+
+document.getElementsByClassName("tablink9")[0].click();
+
+function openPrograma9(evt, programa9Name) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("programa9");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink9");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(programa9Name).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+}
+
+
+
+document.getElementsByClassName("tablink10")[0].click();
+
+function openPrograma10(evt, programa10Name) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("programa10");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink10");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(programa10Name).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+}
+
+
+
+document.getElementsByClassName("tablink11")[0].click();
+
+function openPrograma11(evt, programa11Name) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("programa11");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink11");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(programa11Name).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+}
+
+
+document.getElementsByClassName("tablink12")[0].click();
+
+function openPrograma12(evt, programa12Name) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("programa12");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink12");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(programa12Name).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+}
+
+
+document.getElementsByClassName("tablink13")[0].click();
+
+function openPrograma13(evt, programa13Name) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("programa13");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink13");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(programa13Name).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+}
+
+
+document.getElementsByClassName("tablink14")[0].click();
+
+function openPrograma14(evt, programa14Name) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("programa14");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink14");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(programa14Name).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+}
+
+
+document.getElementsByClassName("tablink15")[0].click();
+
+function openPrograma15(evt, programa15Name) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("programa15");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink15");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(programa15Name).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+}

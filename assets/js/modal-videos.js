f51b08c5cb754070e6facac62f82a44b855e9066
desirefansub <97476151+desirefansub@users.noreply.github.com>
2022-02-20 14:52:05 -0300

    Update modal-videos.js

--- assets/js/modal-videos.js
+++ assets/js/modal-videos.js
@@ -55,6 +55,21 @@ function openPrograma4(evt, programa4Name) {
 
 
 
+document.getElementsByClassName("tablink5")[0].click();
+
+function openPrograma5(evt, programa5Name) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("programa5");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink5");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].classList.remove("w3-light-grey");
+  }
+  document.getElementById(programa5Name).style.display = "block";
+  evt.currentTarget.classList.add("w3-light-grey");
+}
 
 
 
